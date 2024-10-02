0f667df79fad191d3c74c0313e0aef67e9fde8fc
diana.iordan <diana.iordan>
2024-10-02 18:20:30 +0300

    Stage 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
 import com.google.gson.Gson;
+
+import javax.sound.midi.Soundbank;
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
@@ -6,7 +8,6 @@ public class Main {
 
   public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.out.println("Logs from your program will appear here!");
     String command = args[0];
     if("decode".equals(command)) {
       //  Uncomment this block to pass the first stage
