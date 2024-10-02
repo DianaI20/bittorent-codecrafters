ccb0d66b4be53b94202b172a94d018183454ff41
diana.iordan <diana.iordan>
2024-10-02 19:16:28 +0300

    Stage 3 - decoding numeric values

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,50 @@
 import com.google.gson.Gson;
 
 import javax.sound.midi.Soundbank;
+import java.text.NumberFormat;
+import java.text.ParseException;
 // import com.dampcake.bencode.Bencode; - available if you need it!
 
 public class Main {
-  private static final Gson gson = new Gson();
+    private static final Gson gson = new Gson();
 
-  public static void main(String[] args) throws Exception {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    String command = args[0];
-    if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
-        String bencodedValue = args[1];
-        String decoded;
-        try {
-          decoded = decodeBencode(bencodedValue);
-        } catch(RuntimeException e) {
-          System.out.println(e.getMessage());
-          return;
+    public static void main(String[] args) throws Exception {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        String command = args[0];
+        if ("decode".equals(command)) {
+            //  Uncomment this block to pass the first stage
+            String bencodedValue = args[1];
+            Object decoded;
+            try {
+                decoded = decodeBencode(bencodedValue);
+            } catch (RuntimeException e) {
+                System.out.println(e.getMessage());
+                return;
+            }
+            System.out.println(gson.toJson(decoded));
+        } else {
+            System.out.println("Unknown command: " + command);
         }
-        System.out.println(gson.toJson(decoded));
 
-    } else {
-      System.out.println("Unknown command: " + command);
     }
 
-  }
+    static Object decodeBencode(String bencodedString) throws Exception {
+        if (Character.isDigit(bencodedString.charAt(0))) {
+            int firstColonIndex = 0;
+            for (int i = 0; i < bencodedString.length(); i++) {
+                if (bencodedString.charAt(i) == ':') {
+                    firstColonIndex = i;
+                    break;
+                }
+            }
+            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
 
-  static String decodeBencode(String bencodedString) {
-    if (Character.isDigit(bencodedString.charAt(0))) {
-      int firstColonIndex = 0;
-      for(int i = 0; i < bencodedString.length(); i++) { 
-        if(bencodedString.charAt(i) == ':') {
-          firstColonIndex = i;
-          break;
+            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
+        } else {
+            if (bencodedString.charAt(0) == 'i' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
+                return Long.parseLong(bencodedString.substring(1, bencodedString.length() - 1));
+            }
+            throw new Exception("Something went wrong");
         }
-      }
-      int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-      return bencodedString.substring(firstColonIndex+1, firstColonIndex+1+length);
-    } else {
-      throw new RuntimeException("Only strings are supported at the moment");
     }
-  }
-  
 }
