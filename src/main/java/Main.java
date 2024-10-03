9a602918d75d4cd171bdf46dcba0349f7ccd9ad6
diana.iordan <diana.iordan>
2024-10-03 19:13:37 +0300

    Stage 6 - Parsing torrent file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,39 +1,52 @@
 import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
 import com.dampcake.bencode.Bencode;// - available if you need it!
+
+import javax.sound.midi.Soundbank;
 
 public class Main {
     private static final Gson gson = new Gson();
+    private static final Bencode bencode = new Bencode();
 
     public static void main(String[] args) throws Exception {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         String command = args[0];
-        if ("decode".equals(command)) {
-            //  Uncomment this block to pass the first stage
-            String bencodedValue = args[1];
-            Object decoded;
-            try {
-                decoded = decodeBencode(bencodedValue);
-            } catch (RuntimeException e) {
-                System.out.println(e.getMessage());
-                return;
-            }
-            System.out.println(gson.toJson(decoded));
-        } else {
-            System.out.println("Unknown command: " + command);
+        switch (command) {
+            case "decode":
+                String bencodedValue = args[1];
+                Object decoded;
+                try {
+                    decoded = decodeBencode(bencodedValue);
+                } catch (RuntimeException e) {
+                    System.out.println(e.getMessage());
+                    return;
+                }
+                System.out.println(gson.toJson(decoded));
+                break;
+            case "info":
+                Object parsedTorrentFile = parseTorrentFile(args[1]);
+                System.out.println(gson.toJson(parsedTorrentFile));
+            default:
+                System.out.println("Unknown command: " + command);
         }
-
     }
 
+    //TODO : implemement Bencode object algorithms
+
     static Object decodeBencode(String bencodedString) {
-        Bencode bencode =  new Bencode();
+
         if (Character.isDigit(bencodedString.charAt(0))) {
             return bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.STRING);
-
         }
 
         if (bencodedString.charAt(0) == 'i' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
@@ -49,61 +62,26 @@ public class Main {
             return bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.DICTIONARY);
         }
 
+        return "Invalid decode string";
+    }
+
+    static Object parseTorrentFile(String fileName) {
+        try {
+
+            byte[] input = Files.readAllBytes(Paths.get(fileName));
+
+            Map<String, Object> decoded = bencode.decode(input, Type.DICTIONARY);
+            List<Object> output = new ArrayList<>();
+            output.add("Tracker URL: " + decoded.get("announce"));
+            output.add("Length: " + ((Map<String, Object>) decoded.get("info")).get("length"));
+
+            return output;
+        } catch (FileNotFoundException e) {
+            System.out.println("File was not found");
+        } catch (IOException e) {
+            System.out.println("Cannot read from file");
+        }
+
         return null;
-    }
-
-    private static String decodeString (String bencodedString) {
-        int firstColonIndex = 0;
-        for (int i = 0; i < bencodedString.length(); i++) {
-            if (bencodedString.charAt(i) == ':') {
-                firstColonIndex = i;
-                break;
-            }
-        }
-        int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-
-        return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
-    }
-
-    private static Long decodeNumeric(String bencodedString) {
-        return Long.parseLong(bencodedString.substring(1, bencodedString.length() - 1));
-    }
-
-    private static List<Object> decodeList(String bencodedString) {
-        List<Object> list = new ArrayList<>();
-
-        int index = 0;
-
-        if(bencodedString.isEmpty()){
-            return list;
-        }
-
-        while (index < bencodedString.length()) {
-
-            if (Character.isDigit(bencodedString.charAt(index))) {
-                String decodedString = decodeString(bencodedString);
-                list.add(decodedString);
-                index += decodedString.length() + 2;
-            }
-
-            if (bencodedString.charAt(index) == 'i') {
-                StringBuilder stringBuilder = new StringBuilder();
-                index++;
-                while (bencodedString.charAt(index) != 'e'){
-                    stringBuilder.append(bencodedString.charAt(index));
-                    index++;
-                }
-
-                list.add(Long.parseLong(stringBuilder.toString()));
-                index++;
-            }
-
-            if (bencodedString.charAt(index) == 'l') {
-                while (bencodedString.charAt(index) != 'e'){
-                  // list.add(decodeList());
-                }
-            }
-        }
-        return list;
     }
 }
