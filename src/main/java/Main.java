2732ed6a15cc1b17db202569ca19b0566d3f5e97
Diana Iordan <diana@DESKTOP-QBF5TCN>
2024-10-03 08:58:40 +0300

    Stage 4: Decoding lists

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
+import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
-import javax.sound.midi.Soundbank;
-import java.text.NumberFormat;
-import java.text.ParseException;
-// import com.dampcake.bencode.Bencode; - available if you need it!
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import com.dampcake.bencode.Bencode;// - available if you need it!
 
 public class Main {
     private static final Gson gson = new Gson();
@@ -28,23 +29,77 @@ public class Main {
 
     }
 
-    static Object decodeBencode(String bencodedString) throws Exception {
+    static Object decodeBencode(String bencodedString) {
+        Bencode bencode =  new Bencode();
         if (Character.isDigit(bencodedString.charAt(0))) {
-            int firstColonIndex = 0;
-            for (int i = 0; i < bencodedString.length(); i++) {
-                if (bencodedString.charAt(i) == ':') {
-                    firstColonIndex = i;
-                    break;
+            return bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.STRING);
+
+        }
+
+        if (bencodedString.charAt(0) == 'i' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
+            return bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.NUMBER);
+
+        }
+
+        if (bencodedString.charAt(0) == 'l' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
+            return bencode.decode(bencodedString.getBytes(StandardCharsets.UTF_8), Type.LIST);
+        }
+
+        return null;
+    }
+
+    private static String decodeString (String bencodedString) {
+        int firstColonIndex = 0;
+        for (int i = 0; i < bencodedString.length(); i++) {
+            if (bencodedString.charAt(i) == ':') {
+                firstColonIndex = i;
+                break;
+            }
+        }
+        int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
+
+        return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
+    }
+
+    private static Long decodeNumeric(String bencodedString) {
+        return Long.parseLong(bencodedString.substring(1, bencodedString.length() - 1));
+    }
+
+    private static List<Object> decodeList(String bencodedString) {
+        List<Object> list = new ArrayList<>();
+
+        int index = 0;
+
+        if(bencodedString.isEmpty()){
+            return list;
+        }
+
+        while (index < bencodedString.length()) {
+
+            if (Character.isDigit(bencodedString.charAt(index))) {
+                String decodedString = decodeString(bencodedString);
+                list.add(decodedString);
+                index += decodedString.length() + 2;
+            }
+
+            if (bencodedString.charAt(index) == 'i') {
+                StringBuilder stringBuilder = new StringBuilder();
+                index++;
+                while (bencodedString.charAt(index) != 'e'){
+                    stringBuilder.append(bencodedString.charAt(index));
+                    index++;
+                }
+
+                list.add(Long.parseLong(stringBuilder.toString()));
+                index++;
+            }
+
+            if (bencodedString.charAt(index) == 'l') {
+                while (bencodedString.charAt(index) != 'e'){
+                  // list.add(decodeList());
                 }
             }
-            int length = Integer.parseInt(bencodedString.substring(0, firstColonIndex));
-
-            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length);
-        } else {
-            if (bencodedString.charAt(0) == 'i' && bencodedString.charAt(bencodedString.length() - 1) == 'e') {
-                return Long.parseLong(bencodedString.substring(1, bencodedString.length() - 1));
-            }
-            throw new Exception("Something went wrong");
         }
+        return list;
     }
 }
